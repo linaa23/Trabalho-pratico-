3662a820bb0ab77a0d8ddb7c3170778a7897d7cb
Nicolly Fernandes <133915216+linaa23@users.noreply.github.com>
2024-09-04 12:23:55 -0300

    Delete sistemas.js

--- sistemas.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.item img').forEach(img => {
-    img.addEventListener('click', () => {
-        img.classList.add('active');
-        
-        setTimeout(() => {
-            img.classList.remove('active');
-        }, 300); 
-})});
